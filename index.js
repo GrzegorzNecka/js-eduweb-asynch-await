147cd1aa7c3d2bee85e67d865fb5b558adfa67cb
GrzegorzNecka <strzypior@gmail.com>
2021-05-25 14:09:45 +0200

    1 file modified

--- index.js
+++ index.js
@@ -8,11 +8,10 @@
 
   const getPokemon = handleError(async name => {
     const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+
     return await data.json();
   });
 
   const pokemon = await getPokemon(name);
   console.log(pokemon?.name);
 })();
-
-// currying https://eduweb.pl/programowanie-i-www/javascript/asynchronicznosc-w-javascript-krok-po-kroku - 20 minuta
